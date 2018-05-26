c2eab6ee1038331544f5ee67868edcfe0f79f4ec
zoe <z.zoelec2@gmail.com>
2018-05-26 16:19:21 -0700

    fix: remove DOT cpp library

--- library/library.cpp
+++ library/library.cpp
@@ -8,41 +8,6 @@
 
 using namespace std;
 
-extern "C" DLLEXPORT double *DOT(double A[3][3], double B[3][3]) {
-    int i, j, m, n, p, q = 3;
-    double C[3];
-    
-    for (i = 0; i < m; i++)
-    {
-        C[i] = 0;
-        for (j = 0; j < n; j++)
-            C[i] +=  A[i][j] * B[i][j];
- 
-    }
-    // Printing matrix A //
-    cout << "Matrix A : \n ";
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-            cout << A[i][j] << " ";
-        cout << "\n ";
-    }
- 
-    // Printing matrix B //
-    cout << "Matrix B : \n ";
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-            cout << B[i][j] << " ";
-        cout << "\n ";     
-    }
-    cout << "Dot product : \n ";
-    for (i = 0; i < m; i++)
-       cout << C[i] << " ";
-
-    return C;
-}
-
 /// printd - printf that takes a double prints it as "%f\n", returning 0.
 extern "C" DLLEXPORT double printd(double X) {
   fprintf(stderr, "%f\n", X);
