2f8120d4a0a3dac898e90ed3bfd2e903ff560d8e
zoe <z.zoelec2@gmail.com>
2018-06-03 09:50:18 -0700

    feat: add pstream as submodule

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,41 @@
 #include "gtest/gtest.h"
-#include "input.h"
-#include "../main.cpp"
+#include "../pstream/pstream.h"
+
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd) {
+  std::string command(cmd);
+  command.append(" 2>&1");
+
+  std::array<char, 128> buffer;
+  std::string result;
+  std::shared_ptr<FILE> pipe(popen(command.c_str(), "r"), pclose);
+  if (!pipe) throw std::runtime_error("popen() failed!");
+  while (!feof(pipe.get())) {
+    if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+      result += buffer.data();
+  }
+  return result;
+}
 
 TEST(basicComputation, Addition) {
-  EXPECT_EQ(1, 1);
+	std::string output = exec("../run.sh $SUPERFLOW_DIR/tmp/simple.spr 2");
+  std::string line;
+  std::string lastLine;
+
+  std::stringstream lineStream(output);
+  while (lineStream.good()) {
+    getline(lineStream, lastLine, '\n');
+    if (lastLine != "")
+      line = lastLine;
+  }
+
+  EXPECT_EQ(line, "11.000000");
 }
 
 int main (int argc, char* argv[]) {
