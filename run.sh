c6d085418ee03dbf43357c1813c2982f3ca7b7ab
zoe <z.zoelec2@gmail.com>
2018-06-02 20:21:24 -0700

    feat: use /Users/zoe/Developer/superFlow instead of arbatrary dir

--- run.sh
+++ run.sh
@@ -8,8 +8,10 @@ optcomp=
 optall=
 opt2=
 optd=
+opti=
 for x; do
   if [ "$x" = "all" ]; then optall=1; break; fi
+  if [ "$x" = "i" ]; then opti=1; break; fi
   if [ "$x" = "2" ]; then opt2=1; break; fi
   if [ "$x" = "build" ]; then optbuild=1; break; fi
   if [ "$x" = "compile" ]; then optcomp=1; break; fi
@@ -19,27 +21,45 @@ done
 if [ -n "$optbuild" ] || [ -n "$optall" ]; then
   echo "building... "
 
-  clang++ super.cpp ast/ast.cpp lexer/lexer.cpp parser/parser.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core mcjit native` -O3
+  clang++ $SUPERFLOW_DIR/super.cpp $SUPERFLOW_DIR/ast/ast.cpp $SUPERFLOW_DIR/lexer/lexer.cpp $SUPERFLOW_DIR/parser/parser.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core mcjit native` -O3 -o $SUPERFLOW_DIR/a.out
 fi
 
 if [ -n "$optd" ]; then
   echo "building (debug)... "
 
-  clang++ super.cpp ast/ast.cpp lexer/lexer.cpp parser/parser.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core mcjit native` -O3 -g
+  clang++ $SUPERFLOW_DIR/super.cpp $SUPERFLOW_DIR/ast/ast.cpp $SUPERFLOW_DIR/lexer/lexer.cpp $SUPERFLOW_DIR/parser/parser.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core mcjit native` -O3 -g -o $SUPERFLOW_DIR/a.out
 fi
 
 if [ -n "$optcomp" ] || [ -n "$optall" ] || [ -n "$opt2" ]; then
   echo "compiling... "
 
-  less library/matrix.spr > tmp/out.spr
-  less $1 >> tmp/out.spr
-  ./a.out tmp/out.spr &> out.ll
+  less $SUPERFLOW_DIR/library/matrix.spr > $SUPERFLOW_DIR/tmp/out.spr
+  less $1 >> $SUPERFLOW_DIR/tmp/out.spr
+  $SUPERFLOW_DIR/a.out $SUPERFLOW_DIR/tmp/out.spr &> $SUPERFLOW_DIR/out.ll
 
-  clang++ library/library.cpp out.ll -o built
+  clang++ $SUPERFLOW_DIR/library/library.cpp $SUPERFLOW_DIR/out.ll -o $SUPERFLOW_DIR/built
 fi
 
 if [ -n "$optrun" ] || [ -n "$optall" ] || [ -n "$opt2" ]; then
   echo "running... "
 
-  ./built
+  $SUPERFLOW_DIR/built
+fi
+
+if [ -n "$opti" ]; then
+  echo "generating input for test file... "
+
+  less $SUPERFLOW_DIR/library/matrix.spr > $SUPERFLOW_DIR/tmp/out.spr
+  less $1 >> $SUPERFLOW_DIR/tmp/out.spr
+  $SUPERFLOW_DIR/a.out $SUPERFLOW_DIR/tmp/out.spr &> $SUPERFLOW_DIR/out.ll
+
+  clang++ $SUPERFLOW_DIR/library/library.cpp $SUPERFLOW_DIR/out.ll -S -emit-llvm
+
+  less $SUPERFLOW_DIR/library.ll > $SUPERFLOW_DIR/gen.ll
+  less $SUPERFLOW_DIR/out.ll >> $SUPERFLOW_DIR/gen.ll
+
+  less $SUPERFLOW_DIR/gen.ll > $3
+
+  rm $SUPERFLOW_DIR/library.ll
+  rm $SUPERFLOW_DIR/gen.ll
 fi
